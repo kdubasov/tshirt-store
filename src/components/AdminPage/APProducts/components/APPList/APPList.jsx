c220de07b4113f9c55bd396c1fdb5cb7313de2d3
kdubasov <cergocergo41@gmail.com>
2023-03-05 21:15:58 +0300

    redact APPLItem.jsx and add search to APPList.jsx

--- src/components/AdminPage/APProducts/components/APPList/APPList.jsx
+++ src/components/AdminPage/APProducts/components/APPList/APPList.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./APPList.css";
 import {useGetAllProducts} from "../../../../../pages-hooks/AdminPage/Products/useGetAllProducts.js";
-import {Badge} from "react-bootstrap";
+import {Badge, FormControl} from "react-bootstrap";
 import APPLItem from "./components/APPLItem/APPLItem.jsx";
 
 const APPList = () => {
 
+    //список всех товаров
     const data = useGetAllProducts();
-    console.log(data);
+    const [dataSorted, setDataSorted] = useState([]);
+    // console.log(data);
 
+    //запрос при поиске
+    const [query, setQuery] = useState("");
+
+    useEffect(() => {
+        if (query){//чтобы показывались только те которые содержат в себе запрос
+            setDataSorted(data.filter(elem => elem.title.toLowerCase().includes(query.toLowerCase())))
+        }
+    },[query])
 
     return (
         <div className={"APPList"}>
             <Badge className={"w-100 mb-1"} bg={"success"}>
                 Список товаров ({data.length})
             </Badge>
+
+            {//поиск по товарам
+                Boolean(data.length) &&
+                <FormControl
+                    className={"APPSearch mb-1"}
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
+                    placeholder={"Поиск по товарам..."}
+                />
+            }
 
             {//check categories length for text about no data
                 !data.length &&
@@ -27,7 +47,7 @@ const APPList = () => {
                 Boolean(data.length) &&
                 <div className="content">
                     {
-                        data.map(elem => (
+                        (query ? dataSorted : data).map(elem => (
                             <APPLItem key={elem.id} data={elem} />
                         ))
                     }
--- src/components/AdminPage/APProducts/components/APPList/components/APPLItem/APPLItem.jsx
+++ src/components/AdminPage/APProducts/components/APPList/components/APPLItem/APPLItem.jsx
@@ -1,45 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./APPLItem.css";
 import {Link} from "react-router-dom";
 import {LINK_PRODUCT_PAGE_FNC} from "../../../../../../../constants/links.js";
 import {Button, ButtonGroup, ListGroup, ListGroupItem} from "react-bootstrap";
+import DeleteModal from "../../../../../../../general-components/DeleteModal/DeleteModal.jsx";
 
 const APPLItem = ({data}) => {
 
+    //modal delete
+    const [modalDelete, setModalDelete] = useState(false);
+
     return (
-        <div className={"APPLItem"}>
-            <Link to={LINK_PRODUCT_PAGE_FNC(data.category, data.id)}>
-                {data.title}
-            </Link>
+        <>
+            <div className={"APPLItem"}>
+                <Link to={LINK_PRODUCT_PAGE_FNC(data.category, data.id)}>
+                    {data.title}
+                </Link>
 
-            <ListGroup>
-                <ListGroupItem>
-                    Наличие: {data.availability ? "Да" : "Нет"}
-                </ListGroupItem>
-                <ListGroupItem>
-                    Дата: {data.date}
-                </ListGroupItem>
-                <ListGroupItem>
-                    Цена: {data.price}
-                </ListGroupItem>
-                <ListGroupItem>
-                    Скидка: {data.sale || "Нет"}
-                </ListGroupItem>
-            </ListGroup>
+                <ListGroup>
+                    <ListGroupItem>
+                        Наличие: {data.availability ? "Да" : "Нет"}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        Дата: {data.date}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        Цена: {data.price}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        Скидка: {data.sale || "Нет"}
+                    </ListGroupItem>
+                </ListGroup>
 
-            <div className="btn-container">
-                <ButtonGroup>
-                    <Button size={"sm"} variant={"secondary"}>
-                        Редактировать
-                    </Button>
+                <div className="btn-container">
+                    <ButtonGroup>
+                        <Button size={"sm"} variant={"secondary"}>
+                            Редактировать
+                        </Button>
 
-                    <Button size={"sm"} variant={"danger"}>
-                        Удалить
-                    </Button>
-                </ButtonGroup>
+                        <Button onClick={() => setModalDelete(true)} size={"sm"} variant={"danger"}>
+                            Удалить
+                        </Button>
+                    </ButtonGroup>
+                </div>
             </div>
 
-        </div>
+            {/*delete category modal*/}
+            <DeleteModal
+                show={modalDelete}
+                onHide={() => setModalDelete(false)}
+                data={data}
+            />
+        </>
     );
 };
 

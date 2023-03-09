9454306a7041328e89973bb787d42f28b5af9a9e
integ <cergocergo41@gmail.com>
2023-03-09 15:48:44 +0300

    redact FavoritesPage.jsx and add FavoritesPage.css

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import {Badge, Container} from "react-bootstrap";
+import {useGetFavorites} from "../../pages-hooks/ProductPage/favorites/useGetFavorites.js";
+import {useUserAuth} from "../../context-providers/AuthContextProvider.jsx";
+import ProductCard from "../../general-components/ProductCard/ProductCard.jsx";
+import "./FavoritesPage.css";
 
 const FavoritesPage = () => {
+
+    const { user } = useUserAuth();
+    const data = useGetFavorites(user.uid);
+
     return (
         <Container className={"FavoritesPage py-3"}>
             <Badge>Избранные товары</Badge>
+
+            {//if !products in favorites
+                !data.length &&
+                <h6>Список товаров пуст!</h6>
+            }
+
+            <div className="content-products">
+                {
+                    Boolean(data.length) &&
+                    data.map(elem => (
+                        <ProductCard key={elem.id} data={elem} />
+                    ))
+                }
+            </div>
+
         </Container>
     );
 };

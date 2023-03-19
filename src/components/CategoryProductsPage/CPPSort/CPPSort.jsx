b448080f05e515c118f5f9337bb685b3ba87244e
kdubasov <cergocergo41@gmail.com>
2023-03-19 19:30:01 +0300

    add sort for products

--- src/components/CategoryProductsPage/CPPSort/CPPSort.jsx
+++ src/components/CategoryProductsPage/CPPSort/CPPSort.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import {DropdownButton, Dropdown} from "react-bootstrap";
 import {CPPSortData} from "./CPPSortData.js";
 
-const CPPSort = () => {
+const CPPSort = ({sort,setSort}) => {
 
     return (
         <DropdownButton size={"sm"} title="Сортировать" className={"my-2"}>
             {
                 CPPSortData.map(elem => (
-                    <Dropdown.Item key={elem.id}>
+                    <Dropdown.Item
+                        onClick={() => setSort(elem.value)}
+                        key={elem.id}
+                        active={elem.value === sort}
+                    >
                         {elem.text}
                     </Dropdown.Item>
                 ))
+            }
+            {
+                sort && sort !== "default" &&
+                <Dropdown.Item onClick={() => setSort("default")}>
+                    Сбросить фильтры
+                </Dropdown.Item>
             }
         </DropdownButton>
     );
--- src/pages/CategoryProductsPage/CategoryProductsPage.jsx
+++ src/pages/CategoryProductsPage/CategoryProductsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Badge, Container} from "react-bootstrap";
 import {useGetCategoryProducts} from "../../pages-hooks/AdminPage/Products/useGetCategoryProducts.js";
 import {useLocation} from "react-router-dom";
@@ -9,9 +9,44 @@ const CategoryProductsPage = () => {
 
     const location = useLocation();
     const productsList = useGetCategoryProducts(location.pathname);
+    const [products, setProducts] = useState([]);
+    const [sort, setSort] = useState("");
+
+    const handleSort = () => {
+        if (sort === "new"){
+            setProducts(products.sort((a,b) => b.dateNoRedact - a.dateNoRedact));
+        }
+        if (sort === "old"){
+            setProducts(products.sort((a,b) => a.dateNoRedact - b.dateNoRedact));
+        }
+        if (sort === "cheap"){
+            setProducts(products.sort((a,b) => Number(a.price) - Number(b.price)));
+        }
+        if (sort === "expensive"){
+            setProducts(products.sort((a,b) => Number(b.price) - Number(a.price)));
+        }
+        if (sort === "sale"){
+            setProducts(products.sort((a,b) => Number(b.sale) - Number(a.sale)));
+        }
+        if (sort === "availability"){
+            setProducts(products.filter(elem => elem.availability));
+        }
+        if (sort === "default"){
+            setProducts(productsList);
+        }
+    }
+
+    useEffect(() => {
+        //если товары еще не занесены в стейт то заносим
+        if (productsList.length && !products.length) setProducts(productsList);
+
+        //если есть метод сортировки то добавляем функцию
+        if (sort) handleSort();
+    }, [sort,productsList.length,products.length]);
+
 
     //если товаров нет то выводим
-    if (!productsList.length){
+    if (!products.length){
         return (
             <Container className={"CategoryProductsPage py-3"}>
                 <Badge>Товары категории</Badge>
@@ -29,11 +64,13 @@ const CategoryProductsPage = () => {
             <Badge>Товары категории</Badge>
 
             {//если кол-во товаров категории больше трех то показываем сортировку
-                productsList.length > 3 &&
-                <CPPSort />
+                products.length > 3 &&
+                <CPPSort sort={sort} setSort={setSort} />
             }
 
-            <CPPListProducts data={productsList} />
+            <CPPListProducts
+                data={products}
+            />
         </Container>
     )
 };

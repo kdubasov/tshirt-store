d538a5a98bc2544424c0d0f08a679f6c1b1e03ba
kdubasov <cergocergo41@gmail.com>
2023-04-03 23:56:42 +0300

    redact OrderPage.jsx

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -4,10 +4,26 @@ import {useGetBasket} from "../../pages-hooks/ProductPage/basket/useGetBasket.js
 import BPOrderData from "../../components/BasketPage/BPOrderData/BPOrderData.jsx";
 import "./OrderPage.css";
 import OpForm from "../../components/OrderPage/OPForm/OPForm.jsx";
+import {Link} from "react-router-dom";
+import {LINK_CATEGORIES} from "../../constants/links.js";
 
 const OrderPage = () => {
 
+    //корзина
     const basketData = useGetBasket();
+
+    //если корзина пуста то выводим это
+    if (!basketData.length){
+        return (
+            <Container className={"OrderPage py-3"}>
+                <Badge>Оформление заказа</Badge>
+                <p className={"m-0"}>
+                    Ваша корзина пуста поэтому вы не можете оформить заказ!
+                </p>
+                <Link to={LINK_CATEGORIES}>К категориям</Link>
+            </Container>
+        )
+    }
 
     return (
         <Container className={"OrderPage py-3"}>
